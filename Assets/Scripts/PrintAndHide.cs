e862a58a56419fe5290a7c088be0b7575c8a3304
shu lok <shu.lok@embraiz.com>
2023-08-30 21:41:51 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
+    public int rand; //random number
     // Start is called before the first frame update
     public Renderer rend;
 
     void Start()
     {
-        
+        rand = Random.Range(150, 251);
     }
 
     // Update is called once per frame
@@ -18,6 +19,15 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
-        
+
+        if (i == 100 && gameObject.CompareTag("Red"))
+        {
+            gameObject.SetActive(false); // Deactivate the GameObject
+        }
+        if (i == rand && gameObject.CompareTag("Blue"))
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+
     }
 }
