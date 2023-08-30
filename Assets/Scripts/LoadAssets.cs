019b22ab354a36ca48ac3ba544a715fedc93ddd0
shu lok <shu.lok@embraiz.com>
2023-08-30 16:24:41 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,11 +6,18 @@ public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
     // Start is called before the first frame update
+    [SerializeField]
+    private GameObject blueObj;
 
-  
     void Start()
     {
-      
+        Application.targetFrameRate = 60;
+        Vector3 redPos = new Vector3(2, 0, 0);
+        Vector3 bluePos = new Vector3(-2, 0, 0);
+        Quaternion rotation = Quaternion.Euler(0, 0, 0);
+        Instantiate(redObj, redPos, rotation);
+        Instantiate(blueObj, bluePos, rotation);
+
     }
     // Update is called once per frame
     void Update()
