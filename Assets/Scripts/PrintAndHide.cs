30b430d69b2a89d0fa8c216bf043aad6d8b04aff
shu lok <shu.lok@embraiz.com>
2023-08-30 21:16:40 +1000

    90 percent complete

---
+++ Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    private int i = 3;
+    // Start is called before the first frame update
+    public Renderer rend;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log(gameObject.name + ":" + i);
+        
+    }
+}
